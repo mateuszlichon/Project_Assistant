e2153d8a0f25b16de47bc987bf1c7e8a776af427
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-18 10:28:41 +0200

    project init

---
+++ src/main/java/com/lichon/ProjectAssistant/ProjectAssistantApplication.java
@@ -0,0 +1,12 @@
+package com.lichon.ProjectAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAssistantApplication.class, args);
+	}
+}
