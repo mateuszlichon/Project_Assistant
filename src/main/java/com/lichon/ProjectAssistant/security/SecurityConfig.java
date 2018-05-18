30f476feba62ac7712088f5b5b91afefe6b55d1b
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-18 10:39:31 +0200

    adds security config

---
+++ src/main/java/com/lichon/ProjectAssistant/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.lichon.ProjectAssistant.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled=false)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/").permitAll()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.anyRequest().authenticated()
+		.and().formLogin()
+		.loginPage("/login")
+		.and().httpBasic()
+		.and()
+		.sessionManagement().maximumSessions(1);
+	}
+}
