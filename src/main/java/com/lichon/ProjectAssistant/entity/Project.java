52fcd65b9e18c47754eecc45cd17e094fbe2b55f
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-18 11:10:58 +0200

    adds relations

--- src/main/java/com/lichon/ProjectAssistant/entity/Project.java
+++ src/main/java/com/lichon/ProjectAssistant/entity/Project.java
@@ -1,24 +1,30 @@
 package com.lichon.ProjectAssistant.entity;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
 public class Project {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	private String name;
-	
+
 	private String Voivodeship;
-	
+
 	@OneToOne
 	private Beneficiary beneficiary;
+
+	@OneToMany
+	private Set<Task> task;
 
 	public Project() {
 		super();
@@ -55,6 +61,13 @@ public class Project {
 	public void setBeneficiary(Beneficiary beneficiary) {
 		this.beneficiary = beneficiary;
 	}
-	
-	
+
+	public Set<Task> getTask() {
+		return task;
+	}
+
+	public void setTask(Set<Task> task) {
+		this.task = task;
+	}
+
 }
