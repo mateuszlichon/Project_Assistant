8aa1c3bccf04b72d77d9157f7923dd2ceaceb567
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-21 13:15:37 +0200

    adjusts views

--- src/main/webapp/resources/js/projects.js
+++ src/main/webapp/resources/js/projects.js
@@ -2,34 +2,6 @@ $(function () {
   var ajax = new Ajax();
   var projectsList = $('.projectsList');
 
-  // function renderProjectsList(endpoint) {
-  //   ajax.ajaxGetCallback(endpoint, function (response) {
-  //     projectsList.empty();
-  //     var data = response.content;
-  //     response.forEach(function (elem) {
-  //       $('.projectsList').append('<tr class="table-active">' +
-  //       '<td>' + elem.voivodeship + '</td>' +
-  //       '<td>' + elem.name + '</td>' +
-  //       '<td>' + elem.beneficiary.name + '</td>' +
-  //       '<td>        <div class="dropdown">' +
-  //           '<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">' +
-  //               'Zadania w projekcie' +
-  //           '</button>' +
-  //           '<div class="dropdown-menu project'+elem.id+'-tasks">' +
-  //             elem.task.forEach(function(task) {
-  //               task.name
-  //               // console.log(task.name) +
-  //               console.log($('.project4-tasks')) +
-  //               $('.project4-tasks').append('<a class="dropdown-item" href="#">' + task.name + '</a>')
-  //             }) +
-  //           '</div>' +
-  //       '</div>' +
-  //       '</td>' +
-  //       '</tr>');
-  //     })
-  //   })
-  // }
-
   function renderProjectsList(endpoint) {
     ajax.ajaxGetCallback(endpoint, function (response) {
       projectsList.empty();
@@ -40,27 +12,34 @@ $(function () {
         '<td>' + elem.name + '</td>' +
         '<td>' + elem.beneficiary.name + '</td>' +
         '<td>        <div class="dropdown">' +
-            '<button class="btn btn-primary project'+elem.id+' tasks" type="button" >' +
+            '<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">' +
                 'Zadania w projekcie' +
             '</button>' +
             '<div class="dropdown-menu project'+elem.id+'-tasks">' +
-              elem.task.forEach(function(task) {
-                task.name
-                // console.log(task.name) +
-                console.log($('.project4-tasks')) +
-                $('.project4-tasks').append('<a class="dropdown-item" href="#">' + task.name + '</a>')
-              }) +
             '</div>' +
         '</div>' +
         '</td>' +
-        '</tr><div class="project'+elem.id+'-tasks"></div>');
+        '</tr>');
+        renderTastksForMenu(elem)
       })
     })
   }
 
-  projectsList.on('click', '.tasks', function (e) {
-      console.log(e);
-  });
+  function renderTastksForMenu(elem) {
+    elem.task.forEach(function(task) {
+      var partners = "";
+      var numberOfPartners = 1;
+      var coma = "";
+      task.partner.forEach(function(partner) {
+        if (numberOfPartners > 1) {
+          coma = ", "
+        }
+        partners += coma + partner.name;
+        numberOfPartners ++;
+      })
+      $('.project'+elem.id+'-tasks').append('<a class="dropdown-item" href="#">' + task.name + ' <br>('+ partners +')</a>')
+    })
+  }
 
   renderProjectsList('/projects');
 
