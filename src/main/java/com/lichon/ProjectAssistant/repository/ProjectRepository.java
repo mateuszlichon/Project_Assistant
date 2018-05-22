941b11adf47423b8756d270bedf24558ab70b528
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-22 09:23:48 +0200

    supplements project repository

--- src/main/java/com/lichon/ProjectAssistant/repository/ProjectRepository.java
+++ src/main/java/com/lichon/ProjectAssistant/repository/ProjectRepository.java
@@ -1,9 +1,12 @@
 package com.lichon.ProjectAssistant.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lichon.ProjectAssistant.entity.Project;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
+	List<Project> findAllByBeneficiaryId(long id);
 }
