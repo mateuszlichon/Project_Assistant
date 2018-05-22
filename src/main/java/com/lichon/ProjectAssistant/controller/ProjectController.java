40456650047225ae041b68d8055d4fbb276998c6
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-22 12:26:01 +0200

    develops add project method to controller

--- src/main/java/com/lichon/ProjectAssistant/controller/ProjectController.java
+++ src/main/java/com/lichon/ProjectAssistant/controller/ProjectController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +34,10 @@ public class ProjectController {
 	@GetMapping("/beneficiary/{id}")
 	private List<Project> getProjectsByBeneficiary(@PathVariable long id) {
 		return projectRepository.findAllByBeneficiaryId(id);
+	}
+	
+	@PostMapping("")
+	private void createBeneficiary(@RequestBody Project project) {
+		projectRepository.save(project);
 	}
 }
