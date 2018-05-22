497271f0fce1d4308aa2707d0649b13867ac4061
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-22 20:30:55 +0200

    develops add task method in controller

--- src/main/java/com/lichon/ProjectAssistant/controller/TaskController.java
+++ src/main/java/com/lichon/ProjectAssistant/controller/TaskController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,11 @@ public class TaskController {
 	@GetMapping("/{id}")
 	private Optional<Task> getTaskById(@PathVariable long id) {
 		return taskRepository.findById(id);
+	}
+	
+	@PostMapping("")
+	private void createTask(@RequestBody Task task) {
+		taskRepository.save(task);
 	}
 
 }
