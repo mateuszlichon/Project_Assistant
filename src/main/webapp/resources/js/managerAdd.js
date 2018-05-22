b6a6d58194bff4644481d6c32702a117dacbffb3
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-22 10:11:06 +0200

    introduces details

--- src/main/webapp/resources/js/managerAdd.js
+++ src/main/webapp/resources/js/managerAdd.js
@@ -7,7 +7,8 @@ $(function() {
       $('#existingBeneficiaries').empty();
       var data = response.content;
       response.forEach(function (elem) {
-        $('#existingBeneficiaries').append('<button type="button" class="btn btn-secondary btn-block beneficiaries-choice" data-beneficiary='+elem.id+'>'+elem.name+'</button>');
+        $('#existingBeneficiaries').append('<button type="button" class="btn btn-secondary btn-block beneficiaries-choice" data-beneficiary='+elem.id+'>'+elem.name+'</button>'+
+      '<div class="beneficiariesDetails" id="beneficiary'+elem.id+'Details"></div><br/>');
       })
     })
   }
@@ -18,7 +19,8 @@ $(function() {
       $('#projectsTasks').empty();
       var data = response.content;
       response.forEach(function (elem) {
-        $('#beneficiariesProjects').append('<button type="button" class="btn btn-info btn-block projects-choice" data-project='+elem.id+'>'+elem.name+'</button>');
+        $('#beneficiariesProjects').append('<button type="button" class="btn btn-info btn-block projects-choice" data-project='+elem.id+'>'+elem.name+'</button>'+
+      '<div class="projectsDetails" id="project'+elem.id+'Details"></div><br/>');
       })
     })
   }
@@ -28,7 +30,8 @@ $(function() {
       $('#projectsTasks').empty();
       var data = response.content;
       response.task.forEach(function (elem) {
-        $('#projectsTasks').append('<button type="button" class="btn btn-warning btn-block projects-choice" data-task='+elem.id+'>'+elem.name+'</button>');
+        $('#projectsTasks').append('<button type="button" class="btn btn-warning btn-block task-choice" data-task='+elem.id+'>'+elem.name+'</button>'+
+      '<div class="tasksDetails" id="task'+elem.id+'Details"></div><br/>');
       })
     })
   }
@@ -44,11 +47,25 @@ $(function() {
   $('#existingBeneficiaries').on('click', '.beneficiaries-choice', function(e) {
     e.preventDefault();
     renderBeneficiariesProjectList('/projects/beneficiary/' + $(e.target).data('beneficiary'));
+    $('.beneficiariesDetails').empty();
+    // $('#beneficiary'+$(e.target).data('beneficiary')+'Details').empty();
+    $('#beneficiary'+$(e.target).data('beneficiary')+'Details').append('<p class="text-center">szczegoly beneficjenta</p>');
   })
 
   $('#beneficiariesProjects').on('click', '.projects-choice', function(e) {
     e.preventDefault();
     renderProjectsTasksList('/projects/' + $(e.target).data('project'));
+    $('.projectsDetails').empty();
+    // $('#beneficiary'+$(e.target).data('beneficiary')+'Details').empty();
+    $('#project'+$(e.target).data('project')+'Details').append('<p class="text-center">szczegoly projektu</p>');
+  })
+
+  $('#projectsTasks').on('click', '.task-choice', function(e) {
+    e.preventDefault();
+    // renderProjectsTasksList('/projects/' + $(e.target).data('project'));
+    $('.tasksDetails').empty();
+    // $('#beneficiary'+$(e.target).data('beneficiary')+'Details').empty();
+    $('#task'+$(e.target).data('task')+'Details').append('<p class="text-center">szczegoly zadania</p>');
   })
 
   renderExistingBeneficiariesList('/beneficiaries');
