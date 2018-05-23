70e22f24e5f4a2e65ad2761bda105ad4c22d9078
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-23 17:58:54 +0200

    adds delete in project controller

--- src/main/java/com/lichon/ProjectAssistant/controller/ProjectController.java
+++ src/main/java/com/lichon/ProjectAssistant/controller/ProjectController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lichon.ProjectAssistant.entity.Project;
+import com.lichon.ProjectAssistant.entity.Task;
 import com.lichon.ProjectAssistant.repository.ProjectRepository;
 
 @RestController
@@ -45,5 +47,11 @@ public class ProjectController {
 	@PutMapping("")
 	private void updateProject(@RequestBody Project project) {
 		projectRepository.save(project);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	private void deleteTask(@PathVariable long id) {
+		Project project = projectRepository.getOne(id);
+		projectRepository.delete(project);
 	}
 }
