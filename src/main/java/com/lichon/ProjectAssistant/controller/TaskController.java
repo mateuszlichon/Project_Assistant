54460b1c3fdbd988606e1ced23d2bc09e0c5000d
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-04 22:20:24 +0200

    resolves task controller

--- src/main/java/com/lichon/ProjectAssistant/controller/TaskController.java
+++ src/main/java/com/lichon/ProjectAssistant/controller/TaskController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lichon.ProjectAssistant.entity.Task;
 import com.lichon.ProjectAssistant.repository.TaskRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
