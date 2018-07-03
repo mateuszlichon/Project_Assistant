775643ddd771b14dac3689914746f2f6ea00a818
mateuszlichon <mateusz.lichon@gmail.com>
2018-07-03 13:00:44 +0200

    adds edit beneficiary function

--- src/main/java/com/lichon/ProjectAssistant/controller/BeneficiaryController.java
+++ src/main/java/com/lichon/ProjectAssistant/controller/BeneficiaryController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lichon.ProjectAssistant.entity.Beneficiary;
+import com.lichon.ProjectAssistant.entity.Project;
 import com.lichon.ProjectAssistant.repository.BeneficiaryRepository;
 
 @CrossOrigin
@@ -36,6 +38,11 @@ public class BeneficiaryController {
 
 	@PostMapping("")
 	private void createBeneficiary(@RequestBody Beneficiary beneficiary) {
+		beneficiaryRepository.save(beneficiary);
+	}
+	
+	@PutMapping("")
+	private void updateBeneficiary(@RequestBody Beneficiary beneficiary) {
 		beneficiaryRepository.save(beneficiary);
 	}
 
